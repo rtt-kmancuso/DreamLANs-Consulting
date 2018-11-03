981c67a4e51cfff538601f50cee34d22dc601fc3
josef <josef.aidt@gmail.com>
2018-11-03 12:40:34 -0500

    initial commit
    added background
    removed icon header
    added eslint + prettier

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,24 @@
+/* eslint-disable */
 import React from 'react'
 import PropTypes from 'prop-types'
 
 const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fa-diamond"></span>
-        </div>
-        <div className="content">
-            <div className="inner">
-                <h1>DreamLANs Consulting</h1>
-                <p>Let us take the hassle out of your tech.</p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Services</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+  <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+    <div className="content">
+      <div className="inner">
+        <h1>DreamLANs Consulting</h1>
+        <p>Let us take the hassle out of your tech.</p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+        <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Services</a></li>
+        <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+        <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+      </ul>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
