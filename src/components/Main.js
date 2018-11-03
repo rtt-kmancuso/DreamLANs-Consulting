a0c185ce1bf29cc7708213dee4a5d5f0e30690e0
josef <josef.aidt@gmail.com>
2018-11-03 14:06:58 -0500

    escaped multi-line strings, set 'here' button to close for now

--- src/components/Main.js
+++ src/components/Main.js
@@ -37,11 +37,22 @@ export default class Main extends Component {
             <img src={pic01} alt="" />
           </span>
           <p>
-            Welcome! DreamLANs Consulting was built with a simple goal in mind and that is to take
-            the hassle out of every aspect of technology that people commonly face. Whether it's
-            basic PC support, or Corporate IT services and even Custom Audio/Video or Automation
-            solutions. We want to take the stress off of your shoulders. Check out some of our work{' '}
-            <a href="#work">here</a>.
+            {
+              "Welcome! DreamLANs Consulting was built with a simple goal in mind and that is to take \
+            the hassle out of every aspect of technology that people commonly face. Whether it's \
+            basic PC support, or Corporate IT services and even Custom Audio/Video or Automation \
+            solutions. We want to take the stress off of your shoulders. Check out some of our work"
+            }{' '}
+            <a
+              href="javascript:;"
+              onClick={() => {
+                return new Promise(resolve => {
+                  this.props.onCloseArticle()
+                })
+              }}>
+              here
+            </a>
+            .
           </p>
           {close}
         </article>
@@ -56,31 +67,37 @@ export default class Main extends Component {
           <span className="image main">
             <img src={pic02} alt="" />
           </span>
-          <p>// IT Consulting</p>
+          <p>{'// IT Consulting'}</p>
           <p>
             Whether you have an established IT infrastructure, or you are looking for guidance on
             where to go, let DreamLANs work with you to find the most practical solution to fit your
             needs.
           </p>
           <br />
-          <p>// Audio/Video/Control Solutions</p>
+          <p>{'// Audio/Video/Control Solutions'}</p>
           <p>
-            It's Saturday afternoon during football season. Do you know what your TV's are doing?
-            Great video can make or break a gathering and we want to make sure than no matter which
-            TV in the house you're piled around, you've got the best seat in the house every time!
+            {
+              "It's Saturday afternoon during football season. Do you know what your TV's are doing? \
+            Great video can make or break a gathering and we want to make sure than no matter which \
+            TV in the house you're piled around, you've got the best seat in the house every time!"
+            }
           </p>
           <p>
-            It's dinner time; the kids are playing, the spouse is out in the shop or the yard and
-            you're ready for some tunes. With a Whole-Home distributed audio system, you can get the
-            jams you want in whatever room you want! This sounds awesome, right!? We have a solution
-            just for you! Give us a call today to discuss your options!
+            {
+              "It's dinner time; the kids are playing, the spouse is out in the shop or the yard and \
+            you're ready for some tunes. With a Whole-Home distributed audio system, you can get the \
+            jams you want in whatever room you want! This sounds awesome, right!? We have a solution \
+            just for you! Give us a call today to discuss your options!"
+            }
           </p>
           <br />
-          <p>// Hardware Installation</p>
+          <p>{'// Hardware Installation'}</p>
           <p>
-            Desktops? Servers? Routers? Custom or proprietary hardware? Retail? TV's and speakers?
-            We do it all. From the smallest gadgets to the largest projectors, TVs and sound
-            systems... We've got you covered.
+            {
+              "Desktops? Servers? Routers? Custom or proprietary hardware? Retail? TV's and speakers? \
+            We do it all. From the smallest gadgets to the largest projectors, TVs and sound \
+            systems... We've got you covered."
+            }
           </p>
 
           {close}
@@ -97,9 +114,11 @@ export default class Main extends Component {
             <img src={pic03} alt="" />
           </span>
           <p>
-            DreamLANs was birthed from the idea that we love tech, and we love helping people. We
-            have over 8 years of professional knowledge in the IT and Audio/Video Industries, and
-            we're ready to put it to work for you!{' '}
+            {
+              "DreamLANs was birthed from the idea that we love tech, and we love helping people. We \
+            have over 8 years of professional knowledge in the IT and Audio/Video Industries, and \
+            we're ready to put it to work for you!"
+            }{' '}
           </p>
           {close}
         </article>
